f02b9a20c69f94f2768c95547b2bebefbbb2f3e2
carlmax_here <81373116+carlmax-here@users.noreply.github.com>
2021-05-01 16:25:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -10,8 +10,6 @@ See <i>/help</i> for commands and more details.</b>
 
 
     HELP_MSG = """
-<i>Add me as admin in your group and start filtering :)</i>
-
 
 <b>Basic Commands;</b>
 
@@ -48,13 +46,13 @@ See <i>/help</i> for commands and more details.</b>
 <code>/info userid</code>  -  Shows User Information. Use <code>/info</code> as reply to some message for their details!
 
 
-<b>© @TroJanzHEX</b>
+<b>© @cinemapedika</b>
 """
 
 
-    ABOUT_MSG = """⭕️<b>My Name : TroJanz Filter Bot</b>
+    ABOUT_MSG = """⭕️<b>My Name : Unlimited Filter Bot</b>
 
-⭕️<b>Creater :</b> @TroJanzHEX    
+⭕️<b>Creater :</b> @carlmax_here  
 
 ⭕️<b>Language :</b> <code>Python3</code>
 
